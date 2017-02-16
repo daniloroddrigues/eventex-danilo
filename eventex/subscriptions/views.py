46ee45b6b802e6ebee8d10028fa3c29131721a4c
Danilo Rodrigues <danilo.roddrigues@gmail.com>
2017-02-16 18:03:44 -0200

    FormMixin

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,57 +1,48 @@
 from django.conf import settings
 from django.core import mail
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, resolve_url as r
 from django.template.loader import render_to_string
 from django.views import View
 from django.views.generic import DetailView
 from django.views.generic.base import TemplateResponseMixin
+from django.views.generic.edit import FormMixin
 
 from eventex.subscriptions.forms import SubscriptionForm
 from eventex.subscriptions.models import Subscription
 
 
-class SubscriptionCreate(TemplateResponseMixin, View):
+class SubscriptionCreate(TemplateResponseMixin, FormMixin, View):
     template_name = 'subscriptions/subscription_form.html'
     form_class = SubscriptionForm
 
     def get(self, *args, **kwargs):
+        self.object = None
         return self.render_to_response(self.get_context_data())
 
     def post(self, *args, **kwargs):
         """Valida post, caso seja valido retorna um http response redirect 302,
         se nao retorna um http response 200"""
+        self.object = None
         form = self.get_form()
 
         if not form.is_valid():
             return self.form_invalid(form)
         return self.form_valid(form)
 
-    def form_invalid(self, form):
-        """Form is invalid"""
-        return self.render_to_response(self.get_context_data(form=form))
-
     def form_valid(self, form):
         """Form is valid"""
-        subscription = form.save()
+        self.object = form.save()
 
         _send_mail('subscriptions/subscription_email.txt',
-                   {'subscription': subscription},
+                   {'subscription': self.object},
                    'Confirmaçao do inscriçao',
                    settings.DEFAULT_FROM_EMAIL,
-                   subscription.email)
+                   self.object.email)
 
-        return HttpResponseRedirect(r('subscriptions:detail', subscription.pk))
+        return HttpResponseRedirect(self.get_success_url())
 
-    def get_form(self):
-        if self.request.method == 'POST':
-            return self.form_class(self.request.POST)
-        return self.form_class()
-
-    def get_context_data(self, **kwargs):
-        context = dict(kwargs)
-        context.setdefault('form', self.get_form())
-        return context
+    def get_success_url(self):
+        return self.object.get_absolute_url()
 
 
 new = SubscriptionCreate.as_view()
