f01a5fbaf9655cb06966b562d70623257108b726
Danilo Rodrigues <danilo.roddrigues@gmail.com>
2017-02-16 18:12:33 -0200

    BaseCreateView

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,32 +1,21 @@
 from django.conf import settings
 from django.core import mail
-from django.http import HttpResponseRedirect
 from django.template.loader import render_to_string
 from django.views.generic import DetailView
 from django.views.generic.base import TemplateResponseMixin
-from django.views.generic.edit import ModelFormMixin, ProcessFormView
+from django.views.generic.edit import BaseCreateView
 
 from eventex.subscriptions.forms import SubscriptionForm
 from eventex.subscriptions.models import Subscription
 
 
-class SubscriptionCreate(TemplateResponseMixin, ModelFormMixin, ProcessFormView):
+class SubscriptionCreate(TemplateResponseMixin, BaseCreateView):
     template_name = 'subscriptions/subscription_form.html'
     form_class = SubscriptionForm
 
-    def get(self, *args, **kwargs):
-        self.object = None
-        return super().get(*args, **kwargs)
-
-    def post(self, *args, **kwargs):
-        """Valida post, caso seja valido retorna um http response redirect 302,
-        se nao retorna um http response 200"""
-        self.object = None
-        return super().post(*args, **kwargs)
-
     def form_valid(self, form):
         """Form is valid"""
-        self.object = form.save()
+        response = super().form_valid(form)
 
         _send_mail('subscriptions/subscription_email.txt',
                    {'subscription': self.object},
@@ -34,7 +23,7 @@ class SubscriptionCreate(TemplateResponseMixin, ModelFormMixin, ProcessFormView)
                    settings.DEFAULT_FROM_EMAIL,
                    self.object.email)
 
-        return HttpResponseRedirect(self.get_success_url())
+        return response
 
 
 new = SubscriptionCreate.as_view()
