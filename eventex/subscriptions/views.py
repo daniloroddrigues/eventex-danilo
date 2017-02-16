0a426f1fc6646fd9e15a57a9eeac0c9045cb1bed
Danilo Rodrigues <danilo.roddrigues@gmail.com>
2017-02-16 17:26:06 -0200

    SubscriptionCreate

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -3,43 +3,39 @@ from django.core import mail
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, resolve_url as r
 from django.template.loader import render_to_string
+from django.views import View
 from django.views.generic import DetailView
 
 from eventex.subscriptions.forms import SubscriptionForm
 from eventex.subscriptions.models import Subscription
 
 
-def new(request):
-    if request.method == 'POST':
-        return create(request)
+class SubscriptionCreate(View):
+    def get(self, *args, **kwargs):
+        return render(self.request, 'subscriptions/subscription_form.html',
+                      {'form': SubscriptionForm()})
 
-    return empty_form(request)
+    def post(self, *args, **kwargs):
+        """Valida post, caso seja valido retorna um http response redirect 302,
+        se nao retorna um http response 200"""
+        form = SubscriptionForm(self.request.POST)
+
+        if not form.is_valid():
+            return render(self.request, 'subscriptions/subscription_form.html',
+                          {'form': form})
+
+        subscription = form.save()
+
+        _send_mail('subscriptions/subscription_email.txt',
+                   {'subscription': subscription},
+                   'Confirmaçao do inscriçao',
+                   settings.DEFAULT_FROM_EMAIL,
+                   subscription.email)
+
+        return HttpResponseRedirect(r('subscriptions:detail', subscription.pk))
 
 
-def empty_form(request):
-    return render(request, 'subscriptions/subscription_form.html',
-                  {'form': SubscriptionForm()})
-
-
-def create(request):
-    """Valida post, caso seja valido retorna um http response redirect 302,
-    se nao retorna um http response 200"""
-    form = SubscriptionForm(request.POST)
-
-    if not form.is_valid():
-        return render(request, 'subscriptions/subscription_form.html',
-                      {'form': form})
-
-    subscription = form.save()
-
-    _send_mail('subscriptions/subscription_email.txt',
-               {'subscription': subscription},
-               'Confirmaçao do inscriçao',
-               settings.DEFAULT_FROM_EMAIL,
-               subscription.email)
-
-    return HttpResponseRedirect(r('subscriptions:detail', subscription.pk))
-
+new = SubscriptionCreate.as_view()
 
 detail = DetailView.as_view(model=Subscription)
 
