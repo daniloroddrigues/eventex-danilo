3afa229fade28424cc5b1b1584f03e2dc79e6ef3
Danilo Rodrigues <danilo.roddrigues@gmail.com>
2017-02-16 18:49:44 -0200

    Extrair emails padrão para uma classe EmailCreateMixin

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -8,26 +8,56 @@ from eventex.subscriptions.forms import SubscriptionForm
 from eventex.subscriptions.models import Subscription
 
 
-class SubscriptionCreate(CreateView):
+class EmailCreateMixin:
+    email_to = None
+    email_context_name = None
+    email_template_name = None
+    email_from = settings.DEFAULT_FROM_EMAIL
+    email_subject = ''
+
+    def send_email(self):
+        # Send subscription email
+        template_name = self.get_email_template_name()
+        context = self.get_email_context_data()
+        subject = self.email_subject
+        from_ = self.email_from
+        to = self.get_email_to()
+
+        body = render_to_string(template_name, context)
+        return mail.send_mail(subject, body, from_, [from_, to])
+
+    def get_email_template_name(self):
+        if self.email_template_name:
+            return self.email_template_name
+        meta = self.object._meta
+        return '{}/{}_email.txt'.format(meta.app_label, meta.model_name)
+
+    def get_email_context_data(self, **kwargs):
+        context = dict(kwargs)
+        context.setdefault(self.get_email_context_name(), self.object)
+        return context
+
+    def get_email_context_name(self):
+        if self.email_context_name:
+            return self.email_context_name
+        return self.object._meta.model_name
+
+    def get_email_to(self):
+        if self.email_to:
+            return self.email_to
+        return self.object.email
+
+
+class SubscriptionCreate(EmailCreateMixin, CreateView):
     model = Subscription
     form_class = SubscriptionForm
+    email_subject = 'Confirmaçao do inscriçao'
 
     def form_valid(self, form):
         """Form is valid"""
         response = super().form_valid(form)
         self.send_email()
         return response
-
-    def send_email(self):
-        # Send subscription email
-        template_name = 'subscriptions/subscription_email.txt'
-        context = {'subscription': self.object}
-        subject = 'Confirmaçao do inscriçao'
-        from_ = settings.DEFAULT_FROM_EMAIL
-        to = self.object.email
-
-        body = render_to_string(template_name, context)
-        return mail.send_mail(subject, body, from_, [from_, to])
 
 
 new = SubscriptionCreate.as_view()
